c0122e5fd7c83167710cfe8df04151122bed0eee
myxo <klimov.ns@mail.ru>
2022-05-23 22:24:11 +0300

    Add dep

--- neovim/install.sh
+++ neovim/install.sh
@@ -8,5 +8,6 @@ fi
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
+sudo apt-get install curl
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
