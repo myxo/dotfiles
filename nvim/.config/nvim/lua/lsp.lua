c9521dbc348b7f867448586011bf9bd1cd123210
myxo <klimov.ns@mail.ru>
2024-02-18 10:08:51 +0300

    nvim: messing with autocomplete

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -30,7 +30,11 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, {buffer = 0, desc = 'signature'})
   vim.keymap.set("i", "<C-@>", vim.lsp.buf.signature_help, {buffer = 0, desc = 'signature'})
 
-  -- require "lsp_signature".on_attach(signature_setup, bufnr)
+  require "lsp_signature".on_attach({
+    bind = true,
+    handler_opts = { border = "rounded" },
+    doc_lines = 0
+  }, bufnr)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
@@ -49,26 +53,29 @@ local nvim_lsp = require'lspconfig'
 local cmp = require'cmp'
 
 cmp.setup({
-snippet = {
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-  end,
-},
-window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
-  { name = 'luasnip' },
-}, {
-  { name = 'buffer' },
-})
+    completion = {
+        autocomplete = false, -- cmp-config.completion.autocomplete can have TriggerEvent, but I don't understand what I put there
+    },
+    snippet = {
+      expand = function(args) require('luasnip').lsp_expand(args.body) end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      --['.'] = cmp.mapping.complete(), -- this not work
+      --['->'] = cmp.mapping.complete(),-- this not work
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+    --  { name = 'luasnip' },
+    }, {
+      { name = 'buffer' },
+    })
 })
