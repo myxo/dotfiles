a7cd1174e42c7ee67a35f599876f45303b690852
myxo <klimov.ns@mail.ru>
2025-06-17 05:42:43 +0300

    WIP

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -2,26 +2,45 @@ return { -- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	opts = {
+		delay = 500,
 		icons = {
 			mappings = false,
+			keys = {
+				Up = "<Up> ",
+				Down = "<Down> ",
+				Left = "<Left> ",
+				Right = "<Right> ",
+				C = "<C-…> ",
+				M = "<M-…> ",
+				D = "<D-…> ",
+				S = "<S-…> ",
+				CR = "<CR> ",
+				Esc = "<Esc> ",
+				ScrollWheelDown = "<ScrollWheelDown> ",
+				ScrollWheelUp = "<ScrollWheelUp> ",
+				NL = "<NL> ",
+				BS = "<BS> ",
+				Space = "<Space> ",
+				Tab = "<Tab> ",
+				F1 = "<F1>",
+				F2 = "<F2>",
+				F3 = "<F3>",
+				F4 = "<F4>",
+				F5 = "<F5>",
+				F6 = "<F6>",
+				F7 = "<F7>",
+				F8 = "<F8>",
+				F9 = "<F9>",
+				F10 = "<F10>",
+				F11 = "<F11>",
+				F12 = "<F12>",
+			},
+		},
+		spec = {
+			{ "<leader>l", group = "[L]sp" },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>d", group = "[D]iagnostic" },
+			{ "<leader>t", group = "[T]oggle" },
 		},
 	},
-	-- config = function()
-	-- 	require("which-key").setup()
-
-	-- Document existing key chains
-	-- require("which-key").register({
-	-- 	{ "<leader>c", group = "[C]ode" },
-	-- 	{ "<leader>d", group = "[D]ocument" },
-	-- 	{ "<leader>h", group = "Git [H]unk" },
-	-- 	{ "<leader>r", group = "[R]ename" },
-	-- 	{ "<leader>s", group = "[S]earch" },
-	-- 	{ "<leader>t", group = "[T]oggle" },
-	-- 	{ "<leader>w", group = "[W]orkspace" },
-	-- })
-	-- visual mode
-	-- require("which-key").register({
-	-- 	["<leader>h"] = { "Git [H]unk" },
-	-- }, { mode = "v" })
-	-- end,
 }
