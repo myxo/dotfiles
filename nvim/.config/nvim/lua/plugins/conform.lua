aff6501ccb37eaff846eee1c0dcbc1e41cd08218
myxo <klimov.ns@mail.ru>
2025-06-20 00:23:53 +0300

    nvim: small

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,13 @@
 return { -- Autoformat
 	"stevearc/conform.nvim",
 	lazy = false,
+	event = { 'BufWritePre' },
+	cmd = { 'ConformInfo' },
 	keys = {
 		{
 			"<leader>f",
 			function()
-				require("conform").format({ async = true, lsp_fallback = true })
+				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
 			mode = "",
 			desc = "[F]ormat buffer",
@@ -15,10 +17,14 @@ return { -- Autoformat
 		notify_on_error = false,
 		format_on_save = function(bufnr)
 			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				return nil
+			else
+				return {
+					timeout_ms = 500,
+					lsp_format = 'fallback',
+				}
+			end
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
